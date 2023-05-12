e6551d3c34f890468beb730774029f9d4c0646f2
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2023-05-12 17:57:29 +0200

    init

---
+++ java-go/src/inteligenca/Inteligenca.java
@@ -0,0 +1,15 @@
+package inteligenca;
+import splosno.Poteza;
+import logika.Igra;
+
+public class Inteligenca extends splosno.KdoIgra {
+
+    public Inteligenca(){
+        super("play-goh");
+    }
+
+    public Poteza izberiPotezo(Igra igra){
+        // make an intelligent move based on Igra
+    }
+
+}
---
+++ java-go/src/logika/Igra.java
@@ -0,0 +1,17 @@
+package logika;
+import splosno.Poteza;
+
+public class Igra {
+
+    public Igra(){
+        // ustvari novo igro velikosti 9x9
+        // črni začne igro
+    }
+
+    boolean odigraj(Poteza poteza){
+        // oddigra potezo in vrne true, ce je poteza mozna
+        // vrne false, ce poteza ni mozna
+
+        return true;
+    }
+}
